35279c4072d2b67bc3b47b7a42e1baecdf5b625d
Preejith Ponneth <preejithp@gmail.com>
2020-10-17 18:59:52 -0400

    Graphql updates

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
-
-const events = [];
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const schema = buildSchema(`
     type Event {
@@ -13,6 +14,14 @@ const schema = buildSchema(`
         description: String!
         price: Float!
         date: String!
+        creator: User!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        password: String
+        createdEvents: [Event!]
     }
 
     input EventInput {
@@ -22,12 +31,18 @@ const schema = buildSchema(`
         date: String!
     }
 
+    input UserInput {
+        email: String!
+        password: String!
+    }
+
     type RootQuery {
         events: [Event!]!
     }
 
     type RootMutation {
         createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
     }
 
     schema {
@@ -37,37 +52,92 @@ const schema = buildSchema(`
 `);
 
 const root = {
-    events: () => {
-        return events;
-    },
-    createEvent: (args) => {
-        const event = {
-            _id: Math.random().toString(),
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: args.eventInput.date
+  events: () => {
+    return Event.find()
+      .then((events) => {
+        return events.map((event) => {
+          return { ...event._doc, _id: event.id };
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
+  createEvent: (args) => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5f812b20a192e53b6de04850",
+    });
+    let createdEvent;
+    return event
+      .save()
+      .then((response) => {
+        createdEvent = { ...response._doc, _id: response._doc._id.toString() };
+        return User.findById("5f812b20a192e53b6de04850");
+      })
+      .then((user) => {
+        if (!user) {
+          throw new Error("User not found");
         }
-        events.push(event);
-        return event;
-    }
+        user.createdEvents.push(event);
+        return user.save();
+      })
+      .then(() => {
+        return createdEvent;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
+  },
+  createUser: (args) => {
+    return User.findOne({ email: args.userInput.email })
+      .then((user) => {
+        if (user) {
+          throw new Error("User exist already");
+        }
+        return bcrypt.hash(args.userInput.password, 12);
+      })
+      .then((hashedPassword) => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashedPassword,
+        });
+        return user.save();
+      })
+      .then((result) => {
+        return { ...result._doc, password: null, _id: result._id };
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
 };
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oo8yb.mongodb.net/<dbname>?retryWrites=true&w=majority`
-).then(() => {
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oo8yb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
     app.listen(3000);
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-console.log('Running a graphql API Server at PORT 3000');
+console.log("Running a graphql API Server at PORT 3000");
